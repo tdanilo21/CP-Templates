f8a34c06578f5a1b4369a0152d09b87d1eeeaf87
tdanilo21 <83214810+tdanilo21@users.noreply.github.com>
2021-04-26 16:44:38 +0200

    Update min_t.cpp

--- Templates/segtreeTemplates/min_t.cpp
+++ Templates/segtreeTemplates/min_t.cpp
@@ -7,6 +7,7 @@ public:
     static min_t null_v(){ return min_t(-LINF); }
 
     static min_t op(const min_t& a, const min_t& b){ return min_t(min(a.val, b.val)); }
+    // This is currently on set mode but it can be on add.
     void up(const min_t& a){ this->val = a.val; }
     void lazy_op(const min_t& a, int l){ up(a); }
     void Print() const { cout << this->val << sp; }
