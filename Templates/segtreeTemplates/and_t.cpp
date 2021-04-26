783030782d7c8bebbf6015b522bb39cc77712e9c
tdanilo21 <83214810+tdanilo21@users.noreply.github.com>
2021-04-26 18:35:49 +0200

    Create and_t.cpp

---
+++ Templates/segtreeTemplates/and_t.cpp
@@ -0,0 +1,19 @@
+#define ll long long
+class and_t{
+public:
+    ll val;
+
+    and_t(ll val = 0){ this->val = val; }
+    static and_t null_v(){ return and_t((1LL<<63LL)-1LL); }
+
+    static and_t op(const and_t& a, const and_t& b){ return and_t(a.val&b.val); }
+    // This is currently on set mode but it can be on add.
+    void up(const and_t& a){ this->val = a.val; }
+    void lazy_op(const and_t& a, int l){ up(a); }
+    void Print() const { cout << this->val << sp; }
+    void log() const { cerr << this->val << sp; }
+
+    and_t operator =(const and_t& a){ this->val = a.val; return *this; }
+    bool operator ==(const and_t& a) const { return this->val == a.val; }
+    bool operator !=(const and_t& a) const { return this->val != a.val; }
+};
