23b862cfa295eadc10c20518e24681d6d425b637
tdanilo21 <83214810+tdanilo21@users.noreply.github.com>
2021-04-26 19:07:03 +0200

    Update or_t.cpp

--- Templates/segtreeTemplates/or_t.cpp
+++ Templates/segtreeTemplates/or_t.cpp
@@ -7,7 +7,7 @@ public:
     static or_t null_v(){ return or_t(0); }
 
     static or_t op(const or_t& a, const or_t& b){ return or_t(a.val|b.val); }
-    // This is currently on set mode but it can be on add.
+    // This is currently on set mode.
     void up(const or_t& a){ this->val = a.val; }
     void lazy_op(const or_t& a, int l){ up(a); }
     void Print() const { cout << this->val << sp; }
