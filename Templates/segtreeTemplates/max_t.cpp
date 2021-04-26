7574f82ca04b255182a2ca4eb44d99889e99eef4
tdanilo21 <83214810+tdanilo21@users.noreply.github.com>
2021-04-26 13:42:29 +0200

    Create max_t.cpp

---
+++ Templates/segtreeTemplates/max_t.cpp
@@ -0,0 +1,18 @@
+#define ll long long
+class max_t{
+public:
+    ll val;
+
+    max_t(ll val = 0){ this->val = val; }
+    static max_t null_v(){ return max_t(-LINF); }
+
+    static max_t op(const max_t& a, const max_t& b){ return max_t(max(a.val, b.val)); }
+    // This is currently on set mode but it can be on add.
+    void up(const max_t& a){ this->val = a.val; }
+    void lazy_op(const max_t& a, int l){ up(a); }
+    void Print() const { cout << this->val << sp; }
+
+    max_t operator =(const max_t& a){ this->val = a.val; return *this; }
+    bool operator ==(const max_t& a) const { return this->val == a.val; }
+    bool operator !=(const max_t& a) const { return this->val != a.val; }
+};
