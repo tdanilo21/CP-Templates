7c4552504c84b68dd703150f15dc39dcb9fe379a
tdanilo21 <83214810+tdanilo21@users.noreply.github.com>
2021-04-26 18:27:27 +0200

    Update template.cpp

--- Templates/template.cpp
+++ Templates/template.cpp
@@ -36,9 +36,7 @@ void Solve(){
 int main(){
 
     ios::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-    cerr.tie(0);
+    cin.tie(0); cout.tie(0); cerr.tie(0);
     cout << setprecision(12) << fixed;
 
     int t = 1;
