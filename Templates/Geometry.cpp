bdfb06c72cfd9acc3c2104f44fb35233e5cce3ae
tdanilo21 <83214810+tdanilo21@users.noreply.github.com>
2021-05-13 19:04:23 +0200

    Update Geometry.cpp

--- Templates/Geometry.cpp
+++ Templates/Geometry.cpp
@@ -1,4 +1,7 @@
 #define ll long long
+#define ld long double
+#define sp ' '
+#define en '\n'
 int sig(ll a){ return (!a ? 0 : (a<0 ? -1 : 1)); }
 ll gcd(ll a, ll b){ if (!b) return a; return gcd(b, a%b); }
 class Point{
@@ -16,7 +19,7 @@ public:
     static ll Manhattan(const Point& a, const Point& b) { return abs(a.x - b.x) + abs(a.y - b.y); }
     // In Orientation a is center point from which we are watching.
     static ll Orientation(const Point& a, const Point& b, const Point& c) { return sig(b.Norm(a)|c.Norm(a)); }
-    static ld Area(const Point& a, const Point& b, const Point& c) { return abs((ld)(b.Norm(a)|c.Norm(a)) / (ld)2); }
+    static ld Area(const Point* p, int n){ ld area = (p[0].x-p[n-1].x)*(p[0].y+p[n-1].y); for (int i = 0; i < n-1; i++) area += (p[i+1].x-p[i].x)*(p[i].y+p[i+1].y); return area / 2; }
     void Read(){ cin >> this->x >> this->y; }
     void Print() const { cout << this->x << sp << this->y << en; }
     void log() const { cerr << this->x << sp << this->y << endl; }
@@ -30,7 +33,8 @@ public:
 
     void Norm(){ if (this->a<0){ this->a *= -1; this->b *= -1; this->c *= -1; } ll t = gcd(gcd(this->a, this->b), this->c); this->a /= t; this->b /= t; this->c /= t; }
     bool operator==(const Line& l) const { if (this->a^l.a || this->b^l.b || this->c^l.c) return false; return true; }
-    ll f(ll x) const { return (-this->a * x + this->c) / this->b; }
+    ld f(ll x) const { return -(ld)(this->a * x + this->c) / (ld)this->b; }
+    //ld dist(const Point& p) const { Point s = Point(p.x, f(p.x)); } TODO
     ll side(const Point& p) const { return sig(this->a*p.x + this->b*p.y - this->c); }
     void Read(){ cin >> this->a >> this->b >> this->c; Norm(); }
     void Print() const { cout << this->a << sp << this->b << sp << this->c << en; }
