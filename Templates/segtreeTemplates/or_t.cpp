c5b4507923fce794e756b1c7d2afde591a3ef267
tdanilo21 <83214810+tdanilo21@users.noreply.github.com>
2021-06-04 15:40:49 +0200

    Update or_t.cpp

--- Templates/segtreeTemplates/or_t.cpp
+++ Templates/segtreeTemplates/or_t.cpp
@@ -9,10 +9,14 @@ public:
 
     static or_t op(const or_t& a, const or_t& b){ return or_t(a.val|b.val); }
     // This is currently on set mode.
-    void up(const or_t& a){ this->val = a.val; }
+    or_t up(const or_t& a){ return *this = a; }
     void lazy_op(const or_t& a, int l){ up(a); }
 
     or_t operator =(const or_t& a){ this->val = a.val; return *this; }
+    or_t operator +=(const or_t& a) { this->val += a.val; return *this; }
+    or_t operator -=(const or_t& a) { this->val -= a.val; return *this; }
+    or_t operator +(const or_t& a) const { return or_t(this->val + a.val); }
+    or_t operator -(const or_t& a) const { return or_t(this->val - a.val); }
     bool operator ==(const or_t& a) const { return this->val == a.val; }
     bool operator !=(const or_t& a) const { return this->val != a.val; }
     
