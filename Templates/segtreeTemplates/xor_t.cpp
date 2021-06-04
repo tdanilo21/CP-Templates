530f9c2c46bf0ed5c3190969edef43962cd4ee79
tdanilo21 <83214810+tdanilo21@users.noreply.github.com>
2021-06-04 15:42:17 +0200

    Update xor_t.cpp

--- Templates/segtreeTemplates/xor_t.cpp
+++ Templates/segtreeTemplates/xor_t.cpp
@@ -9,10 +9,14 @@ public:
 
     static xor_t op(const xor_t& a, const xor_t& b){ return xor_t(a.val^b.val); }
     // This is currently on set mode.
-    void up(const xor_t& a){ this->val = a.val; }
-    void lazy_op(const xor_t& a, int l){ if (l>1) this->val = 0; else up(a); }
+    xor_t up(const xor_t& a){ return *this = a; }
+    void lazy_op(const xor_t& a, int l){ if (l>1) up(0); else up(a); }
 
     xor_t operator =(const xor_t& a){ this->val = a.val; return *this; }
+    xor_t operator +=(const xor_t& a) { this->val += a.val; return *this; }
+    xor_t operator -=(const xor_t& a) { this->val -= a.val; return *this; }
+    xor_t operator +(const xor_t& a) const { return xor_t(this->val + a.val); }
+    xor_t operator -(const xor_t& a) const { return xor_t(this->val - a.val); }
     bool operator ==(const xor_t& a) const { return this->val == a.val; }
     bool operator !=(const xor_t& a) const { return this->val != a.val; }
     
