4b5c644ca01e90f481d0dbd9023375f5e00f0dd3
tdanilo21 <83214810+tdanilo21@users.noreply.github.com>
2021-06-04 14:44:02 +0200

    Update sum_t.cpp

--- Templates/segtreeTemplates/sum_t.cpp
+++ Templates/segtreeTemplates/sum_t.cpp
@@ -7,12 +7,16 @@ public:
     sum_t(ll val = 0){ this->val = val; }
     static sum_t null_v(){ return sum_t(0); }
 
-    static sum_t op(const sum_t& a, const sum_t& b){ return sum_t(a.val + b.val); }
+    static sum_t op(const sum_t& a, const sum_t& b){ return a + b; }
     // This is currently on set mode but it can be on add.
-    void up(const sum_t& a){ this->val = a.val; }
+    sum_t up(const sum_t& a){ return *this = a; }
     void lazy_op(const sum_t& a, int l){ up(sum_t(a.val * l)); }
 
     sum_t operator =(const sum_t& a){ this->val = a.val; return *this; }
+    sum_t operator +=(const sum_t& a) { this->val += a.val; return *this; }
+    sum_t operator -=(const sum_t& a) { this->val -= a.val; return *this; }
+    sum_t operator +(const sum_t& a) const { return sum_t(this->val + a.val); }
+    sum_t operator -(const sum_t& a) const { return sum_t(this->val - a.val); }
     bool operator ==(const sum_t& a) const { return this->val == a.val; }
     bool operator !=(const sum_t& a) const { return this->val != a.val; }
     
