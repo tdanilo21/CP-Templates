12fbb16a9454050dedb559694cc80f7de4ae7b6d
tdanilo21 <83214810+tdanilo21@users.noreply.github.com>
2021-06-05 18:23:20 +0200

    Update segtree.cpp

--- Templates/segtree.cpp
+++ Templates/segtree.cpp
@@ -68,8 +68,6 @@ public:
     void update(int l, int r, auto x) { update(1, 0, this->n-1, l, r, T(x)); }
     auto query(int l, int r) { if (l>r) return T::null_v().val; return query(1, 0, this->n-1, l, r).val; }
     
-    void PrintTree() const { for (int i = 1; i < 2*this->n; i++) this->tree[i].Print(); cout << en; }
-    void PrintLazy() const { for (int i = 1; i < 2*this->n; i++) this->lazy[i].Print(); cout << en; }
     void logTree() const { for (int i = 1; i < 2*this->n; i++) this->tree[i].log(); cerr << endl; }
     void logLazy() const { for (int i = 1; i < 2*this->n; i++) this->lazy[i].log(); cerr << endl; }
 };
