6aedbb97345fd62e9ce38044323be43b23bf47e9
tdanilo21 <83214810+tdanilo21@users.noreply.github.com>
2021-07-04 17:42:22 +0200

    Create Edmonds-Karp.cpp

---
+++ Templates/GraphTheory/NetworkFlowAlgorithms/Edmonds-Karp.cpp
@@ -0,0 +1,37 @@
+class NetworkFlowSolver : public NetworkFlowSolverBase {
+	private:
+		vector<Edge*> par;
+		ll BFS() {
+			ResetVis();
+			queue<pair<int, ll> > q;
+			q.push(make_pair(this->source, LINF));
+			Visit(this->source);
+			this->par[this->source] = nullptr;
+			while (q.size()){
+				auto [s, flow] = q.front(); q.pop();
+				for (auto e : this->graph[s]){
+					if (!Visited(e->to) && e->RemainingCap()){
+						Visit(e->to);
+						this->par[e->to] = e;
+						q.push(make_pair(e->to, min(flow, e->RemainingCap())));
+						if (e->to == this->sink) return min(flow, e->RemainingCap());
+					}
+				}
+			}
+			return 0;
+		}
+		void Solve() override {
+			this->par = *new vector<Edge*>(this->n);
+			while (ll bottleNeck = BFS()){
+				int s = this->sink;
+				while (s ^ this->source){
+					par[s]->Augment(bottleNeck);
+					s = par[s]->from;
+				}
+				this->maxFlow += bottleNeck;
+			}
+		}
+	public:
+		NetworkFlowSolver() {};
+		NetworkFlowSolver(int n, int source, int sink) { Initialize(n, source, sink); }
+	};
