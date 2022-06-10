cd9a2fe175b87ba8de6a33d26ba413e2084bc64d
tdanilo21 <83214810+tdanilo21@users.noreply.github.com>
2022-06-10 18:05:21 +0200

    Update modop.cpp

--- Templates/modop.cpp
+++ Templates/modop.cpp
@@ -1,27 +1,53 @@
 #define ll long long
-namespace modop{
-    const int mod = (1 ? 1e9+7 : 998244353);
-    vector<ll> fact, ifact;
-    ll norm(ll a){ a%=mod; if(a<0)a+=mod; return a; }
-    ll add(ll a, ll b){ norm(a);norm(b); a+=b; return norm(a); }
-    ll mul(ll a, ll b){ norm(a);norm(b); return ((ll)(a*b))%mod; }
-    ll mpow(ll base, ll exp){
+template <ll mod>
+class mll{
+
+    ll v;
+    void norm(){ v %= mod; if(v<0) v += mod; }
+    mll<mod> inv() const { return  *this ^ (mod-2); }
+    mll<mod> pow(const ll& exp) const {
         if (!exp) return 1;
-        ll t = mpow(base, exp/2);
-        if (exp%2) return mul(mul(t, t), base);
-        return mul(t, t);
+        mll<mod> t = pow(exp/2);
+        if (exp%2) return t*t*v;
+        return t*t;
     }
-    ll inv(ll a){ norm(a); return mpow(a, mod-2); }
-    ll dvs(ll a, ll b){ norm(a); norm(b); return mul(a, inv(b)); }
-    ll nck(ll n, ll k){ norm(n); norm(k); if (k>n) return 0; return mul(fact[n], mul(ifact[k], ifact[n-k])); }
-    #define sadd(a, b) a = add(a, b)
-    #define smul(a, b) a = mul(a, b)
-    #define sdvs(a, b) a = dvs(a, b)
-    void init_fact(int mx){
-        fact = ifact = vector<ll>(mx+10);
-        fact[0] = fact[1] = ifact[0] = ifact[1] = 1;
-        for (int i = 2; i < mx; i++) fact[i] = mul(fact[i-1], i);
-        ifact[mx-1] = inv(fact[mx-1]);
-        for (int i = mx-2; i > 1; i--) ifact[i] = mul(i+1, ifact[i+1]);
+    static vector<mll<mod> > fact, ifact;
+
+public:
+    mll<mod>(ll val = 0){ v = val; norm(); }
+    bool operator==(const mll<mod>& a) const { return v == a.v; }
+    bool operator!=(const mll<mod>& a) const { return v != a.v; }
+    bool operator<(const mll<mod>& a) const { return v < a.v; }
+    bool operator<=(const mll<mod>& a) const { return v <= a.v; }
+    bool operator>(const mll<mod>& a) const { return v > a.v; }
+    bool operator>=(const mll<mod>& a) const { return v >= a.v; }
+
+    mll<mod> operator+(const mll<mod>& a) const { return mll<mod>(v+a.v); }
+    mll<mod> operator-(const mll<mod>& a) const { return mll<mod>(v-a.v); }
+    mll<mod> operator*(const mll<mod>& a) const { return mll<mod>(v*a.v); }
+    mll<mod> operator/(const mll<mod>& a) const { return *this * a.inv(); }
+    mll<mod> operator=(const mll<mod>& a) { v = a.v; norm(); return *this; }
+    mll<mod> operator+=(const mll<mod>& a) { v += a.v; norm(); return *this; }
+    mll<mod> operator-=(const mll<mod>& a) { v -= a.v; norm(); return *this; }
+    mll<mod> operator*=(const mll<mod>& a) { v *= a.v; norm(); return *this; }
+    mll<mod> operator/=(const mll<mod>& a) { v /= a.v; norm(); return *this; }
+    mll<mod> operator^(const ll& a) const { return pow(a); }
+
+    friend istream& operator>>(istream& is, mll<mod>& a){ is >> a.v; return is; }
+    friend ostream& operator<<(ostream& os, const mll<mod>& a){ os << a.v; return os; }
+
+    static void init_fact(int mx){
+        mll<mod>::fact.resize(mx+1);
+        mll<mod>::ifact.resize(mx+1);
+        mll<mod>::fact[0] = mll<mod>::fact[1] = 1;
+        mll<mod>::ifact[0] = mll<mod>::ifact[1] = 1;
+        for (int i = 2; i <= mx; i++) mll<mod>::fact[i] = mll<mod>::fact[i-1] * i;
+        mll<mod>::ifact[mx] = mll<mod>::fact[mx].inv();
+        for (int i = mx-1; i > 1; i--) mll<mod>::ifact[i] = mll<mod>::ifact[i+1] * (i+1);
     }
+    static mll<mod> nck(const mll<mod>& n, const mll<mod>& k){ if (k>n) return 0; return mll<mod>::fact[n.v] * mll<mod>::ifact[k.v] * mll<mod>::ifact[(n-k).v]; }
+    static mll<mod> Fact(const mll<mod>& f){ if (f < 0) return LONG_MIN; return mll<mod>::fact[f.v]; }
 };
+#define mod mll</*set your modulo*/>
+template<> vector<mod> mod::fact = vector<mod>();
+template<> vector<mod> mod::ifact = vector<mod>();
