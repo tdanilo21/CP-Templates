329e0a07279633c87cb186965509c8ca0fabbcd1
tdanilo21 <83214810+tdanilo21@users.noreply.github.com>
2022-11-02 09:51:05 +0100

    Update BIT.cpp

--- Templates/BIT.cpp
+++ Templates/BIT.cpp
@@ -1,36 +1,30 @@
-#define ll long long
 class BIT{
-private:
-    vector<ll> tree;
-    int n;
 
-    ll query(int pos){
+    int n;
+    vector<ll> tree;
+
+    ll query(int pos) const {
         if (pos > n) pos = n;
         ll sum = 0;
-        while (pos > 0){
+        for (; pos; pos -= (pos & (-pos)))
             sum += tree[pos];
-            pos -= pos & (-pos);
-        }
         return sum;
     }
 public:
-    BIT(int s){
-        tree = vector<ll>(s + 1, 0);
-        n = s;
-    }
-
-    ll query(int l, int r){
+    BIT(int s = 0){ Assign(s); }
+    
+    void Assign(int s = 0){ n = s; tree.assign(s+1, 0); }
+    
+    ll query(int l, int r) const {
         l++; r++;
         if (l > r) return 0;
         return query(r) - query(l-1);
     }
-
+    
     void update(int pos, ll x){
         pos++;
         if (pos <= 0) return;
-        while (pos <= n){
+        for (; pos <= n; pos += (pos & (-pos)))
             tree[pos] += x;
-            pos += pos & (-pos);
-        }
     }
 };
