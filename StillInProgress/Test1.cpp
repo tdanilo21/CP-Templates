b7a159651539145453143baf3bcd13943b173550
tdanilo21 <83214810+tdanilo21@users.noreply.github.com>
2022-11-16 13:45:11 +0100

    Update Test1.cpp

--- StillInProgress/Test1.cpp
+++ StillInProgress/Test1.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include "game.h"
 
 #define ll long long
 #define ld long double
@@ -29,65 +30,181 @@
 
 using namespace std;
 
-const ll LINF = 4e18;
-const int mxN = 1e6+10, INF = 2e9;
-ll n, m, ans[mxN];
-vector<array<ll, 3> > a[mxN];
-
-void Solve(){
-
-    cin >> n;
-    for (int i = 0; i < n; i++){
-        ans[i] = LINF;
-        a[i].clear();
+long long gcd2(long long X, long long Y) {
+    long long tmp;
+    while (X != Y && Y != 0) {
+        tmp = X;
+        X = Y;
+        Y = tmp % Y;
     }
-    for (int i = 0; i < n; i++){
-        ri(l); ri(r); ri(c);
-        a[c-1].pb({l, r, i});
-    }
-    multiset<array<ll, 2> > msl, msr;
-    for (int i = 1; i < n; i++){
-        for (auto [l, r, j] : a[i]){
-            msl.insert({l, j});
-            msr.insert({-r, j});
-        }
-    }
-    for (int i = 0; i < n; i++){
-        if (i){
-            for (auto [l, r, j] : a[i-1]){
-                msl.insert({l, j});
-                msr.insert({-r, j});
-            }
-            for (auto [l, r, j] : a[i]){
-                msl.erase(msl.find({l, j}));
-                msr.erase(msr.find({-r, j}));
-            }
-        }
-        for (auto [l, r, j] : a[i]){
-            auto [l1, j1] = *msl.lower_bound({l, 0});
-            auto [r1, j2] = *msr.lower_bound({-r, 0});
-            ll t1 = max(l1-r, 0LL), t2 = max(l-r1, 0LL);
-            smin(ans[j1], t1); smin(ans[j2], t2);
-            smin(ans[j], min(t1, t2));
-        }
-    }
-    for (int i = 0; i < n; i++)
-        cout << ans[i] << sp;
-    cout << en;
+    return X;
 }
 
-int main(){
+struct gcd_t{
 
-    ios::sync_with_stdio(false);
-    cin.tie(0); cout.tie(0); cerr.tie(0);
-    cout << setprecision(12) << fixed;
-    cerr << setprecision(12) << fixed;
-    cerr << "Started!" << endl;
+    ll x;
+    gcd_t(ll v = 0){ update(v); }
+    static gcd_t op(const gcd_t& a, const gcd_t& b){ return gcd_t(gcd2(a.x ,b.x)); }
+    void update(ll v){ x = v; }
+    ll query(){ return x; }
+};
 
-    int t = 1;
-    //cin >> t;
-    while (t--)
-        Solve();
+template<class T>
+struct node{
 
-    return 0;
+    T val;
+    node<T>* ch[2];
+    node<T>(const T& v = T()){ Assign(v); }
+    node<T>(node<T>* p){ Assign(p->val); }
+    ~node<T>(){ delete ch[0]; delete ch[1]; }
+    void Assign(const T& v){
+        val = T(v);
+        ch[0] = ch[1] = nullptr;
+    }
+    node<T>* Ch(int i, const T& a = T()){
+        if (!ch[i]) ch[i] = new node<T>(a);
+        return ch[i];
+    }
+    static node<T>* op(node<T>* a, node<T>* b){
+        if (!a) return new node<T>(b);
+        if (!b) return new node<T>(a);
+        return new node<T>(T::op(a->val, b->val));
+    }
+};
+
+class segtree{
+
+    ll n;
+    node<gcd_t>* root;
+
+    static node<gcd_t>* op(node<gcd_t>* u, node<gcd_t>* v, ll l, ll r){
+        if (!u && !v) return nullptr;
+        if (l == r) return node<gcd_t>::op(u, v);
+        cout << l << sp << r << endl;
+        ll m = (l + r)>>1;
+        node<gcd_t>* a = segtree::op((u ? u->ch[0] : nullptr), (v ? v->ch[0] : nullptr), l, m);
+        node<gcd_t>* b = segtree::op((u ? u->ch[1] : nullptr), (v ? v->ch[1] : nullptr), m+1, r);
+        node<gcd_t>* s = node<gcd_t>::op(a, b);
+        s->ch[0] = a; s->ch[1] = b;
+        return s;
+    }
+
+    gcd_t update(node<gcd_t>* s, ll l, ll r, ll pos, ll x){
+        //cout << l << sp << r << endl;
+        if (l == r){
+            s->val.update(x);
+            return s->val;
+        }
+        int m = (l + r)>>1;
+        gcd_t a = gcd_t(), b = gcd_t();
+        if (pos <= m){
+            a = update(s->Ch(0), l, m, pos, x);
+            if (s->ch[1]) b = s->ch[1]->val;
+        }
+        else{
+            b = update(s->Ch(1), m+1, r, pos, x);
+            if (s->ch[0]) a = s->ch[0]->val;
+        }
+        return s->val = gcd_t::op(a, b);
+    }
+
+    gcd_t query(node<gcd_t>* s, ll l, ll r, ll ql, ll qr) const {
+        if (l > qr || r < ql || !s) return gcd_t();
+        if (l >= ql && r <= qr) return s->val;
+        ll m = (l + r)>>1;
+        gcd_t a = query(s->ch[0], l, m, ql, qr);
+        gcd_t b = query(s->ch[1], m+1, r, ql, qr);
+        return gcd_t::op(a, b);
+    }
+
+public:
+    segtree(ll n = 0){ Assign(n); }
+    segtree(const segtree& st){ Assign(st.n); }
+    void Assign(ll s){
+        n = highpow(s);
+        if (bitcnt(s) > 1) n <<= 1;
+        root = new node<gcd_t>();
+    }
+    ll Size() const { return n; }
+    static segtree op(const segtree& a, const segtree& b){
+        segtree r(a.n);
+        delete r.root;
+        cout << a.n << endl;
+        a.Print(a.root, 0, a.n-1);
+        cout << b.n << endl;
+        b.Print(b.root, 0, b.n-1);
+        r.root = segtree::op(a.root, b.root, 0, a.n-1);
+        return r;
+    }
+    void update(ll pos, ll x){ update(root, 0, n-1, pos, x); }
+    gcd_t query(ll l, ll r) const { return query(root, 0, n-1, l, r); }
+    void Print(node<gcd_t>* s, ll l, ll r) const {
+        cout << l << sp << r << sp << s->val.x << endl;
+        ll m = (l + r)>>1;
+        if (s->ch[0]) Print(s->ch[0], l, m);
+        if (s->ch[1]) Print(s->ch[1], m+1, r);
+    }
+};
+
+class segtree2D{
+
+    ll n;
+    node<segtree>* root;
+
+    segtree update(node<segtree>* s, ll l, ll r, ll pos, array<ll, 2> args){
+        if (l == r){
+            s->val.update(args[0], args[1]);
+            return s->val;
+        }
+        ll m = (l + r)>>1;
+        ll n = s->val.Size();
+        segtree a = segtree(n), b = segtree(n);
+        if (pos <= m){
+            a = update(s->Ch(0, segtree(n)), l, m, pos, args);
+            if (s->ch[1]) b = s->ch[1]->val;
+        }
+        else{
+            b = update(s->Ch(1, segtree(n)), m+1, r, pos, args);
+            if (s->ch[0]) a = s->ch[0]->val;
+        }
+        return s->val = segtree::op(a, b);
+    }
+
+    gcd_t query(node<segtree>* s, ll l, ll r, ll ql, ll qr, array<ll, 2> args) const {
+        if (l > qr || r < ql || !s) return gcd_t();
+        if (l >= ql && r <= qr) return s->val.query(args[0], args[1]);
+        ll m = (l + r)>>1;
+        gcd_t a = query(s->ch[0], l, m, ql, qr, args);
+        gcd_t b = query(s->ch[1], m+1, r, ql, qr, args);
+        return gcd_t::op(a, b);
+    }
+
+public:
+    segtree2D(ll n = 0, const segtree& st = segtree()){ Assign(n, st); }
+    void Assign(ll r, const segtree& st){
+        n = highpow(r);
+        if (bitcnt(r) > 1) n <<= 1;
+        root = new node<segtree>(st);
+    }
+    void update(ll pos, array<ll, 2> args){ update(root, 0, n-1, pos, args); }
+    ll query(ll l, ll r, array<ll, 2> args){ return query(root, 0, n-1, l, r, args).x; }
+};
+
+const ll LINF = 4e18;
+const int mxN = 1e3+10, INF = 2e9;
+ll n, m, a[mxN];
+segtree2D st;
+
+void init(int R, int C){
+    st.Assign(C, segtree(R));
+}
+
+void update(int P, int Q, long long K) {
+    cout << 1 << endl;
+    st.update(Q, {P, K});
+}
+
+long long calculate(int P, int Q, int U, int V) {
+    cout << 2 << endl;
+    return st.query(Q, V, {P, U});
 }
