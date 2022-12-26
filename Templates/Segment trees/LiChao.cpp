7728ae9b8a37fefbf104298efe1170f34a5f925e
tdanilo21 <83214810+tdanilo21@users.noreply.github.com>
2022-12-26 21:15:31 +0100

    Update LiChao.cpp

--- Templates/Segment trees/LiChao.cpp
+++ Templates/Segment trees/LiChao.cpp
@@ -1,46 +1,43 @@
 #define ll long long
-const ll LINF = 4e18;
+#define pb push_back
+const ll LINF = 2e18;
 struct Line{
+ 
     ll k, n;
+    Line(ll _k = 0, ll _n = LINF){ k = _k; n = _n; }
     ll operator()(ll x) const { return k*x+n; }
 };
 class LiChao{
-
-    int ln, rn, n;
+ 
+    ll L, R;
     vector<Line> tree;
     vector<array<int, 2> > ch;
-
-    int Child(int s, int i){ if (!~ch[s][i]) ch[s][i] = ++n; return ch[s][i]; }
-
-    void init(int l, int r, int t){
-        ln = l; rn = r; n = 1;
-        tree.assign(t+10, {0, LINF});
-        ch.assign(t+10, {-1, -1});
-    }
-
+ 
+    int Node(){ tree.pb(Line()); ch.pb({-1, -1}); return tree.size()-1; }
+ 
+    int Child(int s, int i){ if (!~ch[s][i]){ int u = Node(); ch[s][i] = u; } return ch[s][i]; }
+ 
     void Insert(int s, int l, int r, Line f){
-        if (tree[s](l) <= f(l) && tree[s](r) <= f(r)) return;
-        if (tree[s](l) > f(l) && tree[s](r) > f(r)){ swap(tree[s], f); return; }
-
-        if (tree[s](l) > f(l)) swap(tree[s], f);
-
         int m = (l + r)>>1;
-        if (tree[s](m) > f(m)){
-            swap(tree[s], f);
-            Insert(Child(s, 0), l, m, f);
-        }
-        else Insert(Child(s, 1), m+1, r, f);
+        if (f(m) < tree[s](m)) swap(tree[s], f);
+        if (f(l) < tree[s](l)) Insert(Child(s, 0), l, m, f);
+        if (f(r) < tree[s](r)) Insert(Child(s, 1), m+1, r, f);
     }
-
-    ll query(int s, int l, int r, ll x) const {
+ 
+    ll Get(int s, int l, int r, ll x) const {
         if (!~s) return LINF;
         int m = (l + r)>>1;
-        if (x <= m) return min(query(ch[s][0], l, m, x), tree[s](x));
-        return min(query(ch[s][1], m+1, r, x), tree[s](x));
+        if (x <= m) return min(tree[s](x), Get(ch[s][0], l, m, x));
+        else return min(tree[s](x), Get(ch[s][1], m+1, r, x));
     }
-
+ 
 public:
-    LiChao(int l = 0, int r = 0, int t = 0){ init(l, r, t); }
-    void Insert(Line f){ Insert(0, ln, rn, f); }
-    ll query(ll x) const { return query(0, ln, rn, x); }
+    LiChao(ll l, ll r){ Assign(l, r); }
+    void Assign(ll l, ll r){
+        L = l; R = r;
+        tree.clear(); ch.clear();
+        Node();
+    }
+    void Insert(ll k, ll n){ Insert(0, L, R, Line(k, n)); }
+    ll Get(ll x) const { return Get(0, L, R, x); }
 };
