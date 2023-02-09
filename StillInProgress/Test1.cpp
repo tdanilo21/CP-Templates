16cb9e01c5d09ce4c63dd5d925cba4a577bc4f41
tdanilo21 <83214810+tdanilo21@users.noreply.github.com>
2023-02-09 12:03:49 +0100

    Update Test1.cpp

--- StillInProgress/Test1.cpp
+++ StillInProgress/Test1.cpp
@@ -1,5 +1,4 @@
 #include <bits/stdc++.h>
-#include "sorting.h"
 
 #define ll long long
 #define ld long double
@@ -18,8 +17,8 @@
 #define pv(v) for (auto x : v) cout << x << sp; cout << en
 #define all(v) v.begin(), v.end()
 #define rall(v) v.rbegin(), v.rend()
-#define yes cout << "Yes" << en
-#define no cout << "No" << en
+#define yes cout << "YES" << en
+#define no cout << "NO" << en
 #define smin(a, b) a = min(a, b)
 #define smax(a, b) a = max(a, b)
 #define ssort(a, b) if (a < b) swap(a, b)
@@ -30,68 +29,132 @@
 
 using namespace std;
 
+class NetworkFlow{
+
+    struct Edge{
+        int from, to;
+        ll cap, flow, cost;
+        Edge* res;
+
+        Edge(int from, int to, ll cap, ll cost){
+            this->from = from;
+            this->to = to;
+            this->cap = cap;
+            this->cost = cost;
+        }
+
+        bool IsRes() const { return !cap; }
+        ll RemainingCap() const { return cap - flow; }
+        void Augment(ll bottleNeck){ flow += bottleNeck; res->flow -= bottleNeck; }
+    };
+
+    int n, source, sink, _VISITED_;
+    vector<vector<Edge*> > g;
+    vector<int> vis;
+    vector<Edge*> par;
+
+    void Visit(int s){ vis[s] = _VISITED_; }
+    bool Visited(int s) const { return vis[s] == _VISITED_; }
+    void ResetVis(){ _VISITED_++; }
+
+    array<ll, 2> Dijkstra(){
+        ResetVis();
+        vector<ll> dist(n, LONG_MAX);
+        priority_queue<array<ll, 3>,
+                vector<array<ll, 3> >,
+                greater<array<ll, 3> > > pq;
+        dist[source] = 0;
+        pq.push({0, LONG_MAX, source});
+        ll maxFlow = 0;
+        while (pq.size()){
+            auto [d, flow, s] = pq.top(); pq.pop();
+            if (Visited(s)) continue;
+            Visit(s);
+            for (auto e : g[s]){
+                if (e->RemainingCap() && d + e->cost < dist[e->to]){
+                    dist[e->to] = d + e->cost;
+                    par[e->to] = e;
+                    if (e->to == sink) maxFlow = min(flow, e->RemainingCap());
+                    pq.push({dist[e->to], min(flow, e->RemainingCap()), e->to});
+                }
+            }
+        }
+        return {dist[sink], maxFlow};
+    }
+
+public:
+    void assign(int n, int source, int sink){
+        this->n = n;
+        this->source = source;
+        this->sink = sink;
+        _VISITED_ = 1;
+        g.assign(n, vector<Edge*>());
+        vis.assign(n, 0);
+        par.assign(n, nullptr);
+    }
+
+    void AddEdge(int u, int v, ll cap, ll cost){
+        Edge *e = new Edge(u, v, cap, cost), *r = new Edge(v, u, 0, -cost);
+        e->res = r; r->res = e;
+        g[u].pb(e); g[v].pb(r);
+    }
+
+    ll MinCostMaxFlow(){
+        ll maxFlow = 0, minCost = 0;
+        while (1){
+            auto [cost, flow] = Dijkstra();
+            if (!flow) break;
+            int s = sink;
+            while (s != source){
+                par[s]->Augment(flow);
+                s = par[s]->from;
+            }
+            maxFlow += flow;
+            minCost += flow * cost;
+        }
+        return minCost;
+    }
+};
+
 const ll LINF = 1e18;
 const int mxN = 1e6+10, INF = 2e9;
-int n, m, a[mxN], idx[mxN], *s, *x, *y;
-vector<vector<int> > cycles;
-bool vis[mxN];
+ll n, m, a[mxN], b[mxN];
 
-void dfs(int i){
+void Solve(){
 
-    vis[i] = 1;
-    cycles.back().pb(a[i]);
-    if (!vis[a[i]]) dfs(a[i]);
-}
-
-bool Check(int k){
-
-    memset(vis, 0, sizeof(vis));
-    cycles.clear();
+    cin >> n;
     for (int i = 0; i < n; i++)
-        a[i] = s[i];
-    for (int i = 0; i < k; i++)
-        swap(a[x[i]], a[y[i]]);
+        cin >> a[i];
+    vector<int> v;
     for (int i = 0; i < n; i++){
-        if (!vis[i]){
-            cycles.pb({});
-            dfs(i);
-        }
+        ri(x); a[i] -= x;
+        if (a[i] < 0) v.pb(i);
     }
-    return n-cycles.size() <= k;
+    NetworkFlow solver;
+    solver.assign(n+2, 0, n+1);
+    for (int i = 0, j = -1; i < n; i++){
+        if (a[i] <= 0) continue;
+        solver.AddEdge(0, i+1, a[i], 0);
+        while (j < v.size()-1 && v[j+1] < i) j++;
+        if (~j) solver.AddEdge(i+1, v[j]+1, a[i], i-v[j]);
+        if (j < v.size()-1) solver.AddEdge(i+1, v[j+1]+1, a[i], v[j+1]-i);
+    }
+    for (int i : v) solver.AddEdge(i+1, n+1, -a[i], 0);
+    cout << solver.MinCostMaxFlow() << en;
 }
 
-void Swap(int i, int j){
-    swap(idx[s[i]], idx[s[i]]);
-    swap(s[i], s[i]);
-}
+int main(){
 
-int findSwapPairs(int N, int S[], int M, int X[], int Y[], int P[], int Q[]) {
-    n = N; m = M;
-    s = S; x = X; y = Y;
-    int l = 0, r = m-1, ans = m;
-    while (l <= r){
-        int k = (l + r + 1)>>1;
-        if (Check(k)){
-            ans = k;
-            r = k - 1;
-        }
-        else l = k + 1;
-    }
-    Check(ans);
-    vector<array<int, 2> > v;
-    for (auto c : cycles)
-        for (int i = 0; i < c.size()-1; i++)
-            v.pb({c[i], c[i+1]});
-    for (int i = 0; i < n; i++)
-        idx[s[i]] = i;
-    for (int i = 0; i < ans; i++){
-        Swap(x[i], y[i]);
-        if (i < v.size()){
-            P[i] = idx[v[i][0]];
-            Q[i] = idx[v[i][1]];
-        }
-        else P[i] = Q[i] = 0;
-        Swap(P[i], Q[i]);
-    }
-	return ans;
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0); cerr.tie(0);
+    cout << setprecision(12) << fixed;
+    cerr << setprecision(12) << fixed;
+    cerr << "Started!" << endl;
+
+    int t = 1;
+    //cin >> t;
+    while (t--)
+        Solve();
+
+    return 0;
 }
