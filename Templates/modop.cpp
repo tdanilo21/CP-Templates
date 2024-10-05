5ecbe8de663d8f4061e0ed16c07cc0bb492aad9c
tdanilo21 <83214810+tdanilo21@users.noreply.github.com>
2024-10-05 21:35:18 +0200

    Update modop.cpp

--- Templates/modop.cpp
+++ Templates/modop.cpp
@@ -1,20 +1,20 @@
 #define ll long long
 template <ll mod>
-class mll{
+class Mint{
 
     ll v;
     void norm(){ v %= mod; if(v<0) v += mod; }
-    mll<mod> inv() const { return *this ^ (mod-2); }
-    mll<mod> pow(const ll& exp) const {
+    Mint<mod> inv() const { return *this ^ (mod-2); }
+    Mint<mod> pow(const ll& exp) const {
         if (!exp) return 1;
-        mll<mod> t = pow(exp/2);
+        Mint<mod> t = pow(exp/2);
         if (exp%2) return t*t*v;
         return t*t;
     }
-    static vector<mll<mod> > fact, ifact;
+    static vector<Mint<mod> > fact, ifact;
 
 public:
-    mll<mod>(ll val = 0){ v = val; norm(); }
+    Mint<mod>(ll val = 0){ v = val; norm(); }
     bool operator==(const ll& a) const { return v == a; }
     bool operator!=(const ll& a) const { return v != a; }
     bool operator<(const ll& a) const { return v < a; }
@@ -22,39 +22,39 @@ public:
     bool operator>(const ll& a) const { return v > a; }
     bool operator>=(const ll& a) const { return v >= a; }
 
-    mll<mod> operator+(const ll& a) const { return mll<mod>(v+a); }
-    mll<mod> operator-(const ll& a) const { return mll<mod>(v-a); }
-    mll<mod> operator*(const ll& a) const { return mll<mod>(v*a); }
-    mll<mod> operator/(const ll& a) const { return *this * mll<mod>(a).inv(); }
-    mll<mod> operator=(const ll& a) { v = a; norm(); return *this; }
-    mll<mod> operator+=(const ll& a) { v += a; norm(); return *this; }
-    mll<mod> operator-=(const ll& a) { v -= a; norm(); return *this; }
-    mll<mod> operator*=(const ll& a) { v *= a; norm(); return *this; }
-    mll<mod> operator/=(const ll& a) { v *= mll<mod>(a).inv(); norm(); return *this; }
-    mll<mod> operator^(const ll& a) const { return pow(a); }
-    mll<mod> operator^=(const ll& a) { v = pow(a).v; return *this; }
-    mll<mod> operator++() { v++; return *this; }
-    mll<mod> operator++(int) { mll<mod> temp = *this; v++; return temp; }
-    mll<mod> operator--() { v--; return *this; }
-    mll<mod> operator--(int) { mll<mod> temp = *this; v--; return temp; }
+    Mint<mod> operator+(const ll& a) const { return Mint<mod>(v+a); }
+    Mint<mod> operator-(const ll& a) const { return Mint<mod>(v-a); }
+    Mint<mod> operator*(const ll& a) const { return Mint<mod>(v*a); }
+    Mint<mod> operator/(const ll& a) const { return *this * Mint<mod>(a).inv(); }
+    Mint<mod> operator=(const ll& a) { v = a; norm(); return *this; }
+    Mint<mod> operator+=(const ll& a) { v += a; norm(); return *this; }
+    Mint<mod> operator-=(const ll& a) { v -= a; norm(); return *this; }
+    Mint<mod> operator*=(const ll& a) { v *= a; norm(); return *this; }
+    Mint<mod> operator/=(const ll& a) { v *= Mint<mod>(a).inv(); norm(); return *this; }
+    Mint<mod> operator^(const ll& a) const { return pow(a); }
+    Mint<mod> operator^=(const ll& a) { v = pow(a).v; return *this; }
+    Mint<mod> operator++() { v++; return *this; }
+    Mint<mod> operator++(int) { Mint<mod> temp = *this; v++; return temp; }
+    Mint<mod> operator--() { v--; return *this; }
+    Mint<mod> operator--(int) { Mint<mod> temp = *this; v--; return temp; }
 
     operator ll&() { return v; }
     operator ll() const { return v; }
 
     static void init_fact(int mx){
-        mll<mod>::fact.resize(mx+1);
-        mll<mod>::ifact.resize(mx+1);
-        mll<mod>::fact[0] = mll<mod>::fact[1] = 1;
-        mll<mod>::ifact[0] = mll<mod>::ifact[1] = 1;
-        for (int i = 2; i <= mx; i++) mll<mod>::fact[i] = mll<mod>::fact[i-1] * i;
-        mll<mod>::ifact[mx] = mll<mod>::fact[mx].inv();
-        for (int i = mx-1; i > 1; i--) mll<mod>::ifact[i] = mll<mod>::ifact[i+1] * (i+1);
+        Mint<mod>::fact.resize(mx+1);
+        Mint<mod>::ifact.resize(mx+1);
+        Mint<mod>::fact[0] = Mint<mod>::fact[1] = 1;
+        Mint<mod>::ifact[0] = Mint<mod>::ifact[1] = 1;
+        for (int i = 2; i <= mx; i++) Mint<mod>::fact[i] = Mint<mod>::fact[i-1] * i;
+        Mint<mod>::ifact[mx] = Mint<mod>::fact[mx].inv();
+        for (int i = mx-1; i > 1; i--) Mint<mod>::ifact[i] = Mint<mod>::ifact[i+1] * (i+1);
     }
-    static mll<mod> nck(const ll& n, const ll& k){ if (k>n) return 0; return mll<mod>::fact[n] * mll<mod>::ifact[k] * mll<mod>::ifact[(n-k)]; }
-    static mll<mod> Fact(const ll& f){ if (f < 0) return LONG_MIN; return mll<mod>::fact[f]; }
-    static mll<mod> IFact(const ll& f){ if (f < 0) return LONG_MIN; return mll<mod>::ifact[f]; }
+    static Mint<mod> nck(const ll& n, const ll& k){ if (k>n) return 0; return Mint<mod>::fact[n] * Mint<mod>::ifact[k] * Mint<mod>::ifact[(n-k)]; }
+    static Mint<mod> Fact(const ll& f){ if (f < 0) return LONG_MIN; return Mint<mod>::fact[f]; }
+    static Mint<mod> IFact(const ll& f){ if (f < 0) return LONG_MIN; return Mint<mod>::ifact[f]; }
 };
-const ll MOD = /*set your modulo*/;
-#define mod mll<MOD>
-template<> vector<mod> mod::fact = vector<mod>();
-template<> vector<mod> mod::ifact = vector<mod>();
+const ll _MOD_ = /*set your modulo*/;
+#define mint Mint<_MOD_>
+template<> vector<mod> mint::fact = vector<mint>();
+template<> vector<mod> mint::ifact = vector<mint>();
